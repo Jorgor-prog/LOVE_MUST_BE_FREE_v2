04328ee9b5e9020d32738f874e557f327168810b
Jorgor-prog <alpha9274@outlook.com>
2025-08-16 07:58:02 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,13 @@
-export default { experimental: { serverActions: { bodySizeLimit: '4mb' } } };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',         // меньше памяти на рантайме
+  swcMinify: true,              // быстрее и экономнее
+  productionBrowserSourceMaps: false,
+  images: { unoptimized: true },// отключаем sharp и heavy image-оптимизацию
+  experimental: {
+    workerThreads: false,       // не плодим воркеры
+    cpus: 1,                    // 1 CPU хватит и экономнее по ОЗУ
+  },
+};
+
+export default nextConfig;
